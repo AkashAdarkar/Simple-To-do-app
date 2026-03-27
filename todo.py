d44157edf8c3ec1f893271ae2fb1a7311b9069a2
Akash Adarkar <aregret01@gmail.com>
2026-03-27 09:18:07 +0530

    add persistent task storage using tasks.txt

--- todo.py
+++ todo.py
@@ -1,33 +1,54 @@
+task_list = []
+
+
 def list_all_task():
-    for task in task_list:
-        print(task)
-    
+    # print(task_list)
+    with open("tasks.txt","r") as file:
+        for idx,line in enumerate(file,start = 1):
+            print(f"{idx}:{line}")
+# read this from file rather this non-persistent list
+
 
 def add_task():
     task = input("")
     task_list.append(task)
-    list_all_task()
+
+    print(task_list)
+
+    with open ("tasks.txt","w") as file: # save the list to a file
+        file.write("\n".join(task_list)) #list->str
+        file.write("\n")
+    # list_all_task()
+    
+
+
+def delete_task():
+    option = input("enter an index: ")
+    task_list.pop(int(option))
+    
 
 
 def mark_task():
-    pass
+    print("\u2705")
 
 
-
+# over this func we can use time module to mark the date and time of when the task was completed.
 
 while True:
-    task_list = []
     print("A To-Do App")
-    print("""1\t list all tasks \n2\t add new tasks \n3\t mark as completed \n4\t exit""")
-    
+    print(
+        """1\t list all tasks \n2\t add new tasks \n3\t mark as completed \n4\t delete task \n5\t exit"""
+    )
+
     option = input("select an option: ")
     match option:
-        case '1':
+        case "1":
             list_all_task()
-        case '2':
+        case "2":
             add_task()
-        case '3':
+        case "3":
             mark_task()
-        case '4':
+        case "4":
+            delete_task()
+        case "5":
             break
-    
